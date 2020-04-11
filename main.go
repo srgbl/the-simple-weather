d03c94b0749175f09a9fe7376d61c04e0e9073ff
root <root@Fulgens.localdomain>
2020-04-11 20:36:02 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
